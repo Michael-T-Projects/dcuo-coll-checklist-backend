67d272e5f564e2eb0e4fe3851f9a9c66f13dd379
Michael Tillmanns <michael.tillmanns@icloud.com>
2022-03-15 16:05:24 +0100

    Initial commit

---
+++ src/main/java/xyz/michaeltprojects/collchecklist/CollChecklistBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.michaeltprojects.collchecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollChecklistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollChecklistBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/michaeltprojects/collchecklist/CollChecklistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.michaeltprojects.collchecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollChecklistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
