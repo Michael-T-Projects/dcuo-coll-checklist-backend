98f8eee88a434246bef69967f2b1297082cb604a
Michael Tillmanns <michael.tillmanns@icloud.com>
2022-03-16 14:43:51 +0100

    Added categorires boundary layer

---
+++ src/main/java/xyz/michaeltprojects/collchecklist/boundary/api/v1/category/CategoryResource.java
@@ -0,0 +1,29 @@
+package xyz.michaeltprojects.collchecklist.boundary.api.v1.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.michaeltprojects.collchecklist.control.category.CategoryService;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryResource {
+
+    private final CategoryService service;
+    private final CategoryDtoMapper mapper;
+
+    private final static String DEFAULT_MEDIA_TYPE = MediaType.APPLICATION_JSON_VALUE;
+
+    @GetMapping(produces = DEFAULT_MEDIA_TYPE)
+    public ResponseEntity<Collection<CategoryDto>> findAll() {
+        return ResponseEntity.ok(service.findAll().stream().map(mapper::map).collect(Collectors.toList()));
+    }
+
+}
