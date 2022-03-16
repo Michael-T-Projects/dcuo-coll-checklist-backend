cf57e40288b7208692665f753eabb847882cedbf
Michael Tillmanns <michael.tillmanns@icloud.com>
2022-03-16 14:23:21 +0100

    Added category persistence

--- src/main/java/xyz/michaeltprojects/collchecklist/security/persistence/UserRepository.java
+++ src/main/java/xyz/michaeltprojects/collchecklist/security/persistence/UserRepository.java
@@ -1,10 +1,12 @@
 package xyz.michaeltprojects.collchecklist.security.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     Optional<UserEntity> findByEmail(String email);
